1696b17456ef3d7d9f45e90e2076976d9fbe8a90
Shueh Chou Lu <wu0dj2k7ao3@gmail.com>
2021-09-03 14:19:02 +0800

    Build page TOC only has at least 2 headers

--- pagetoc.js
+++ pagetoc.js
@@ -4,6 +4,10 @@ window.addEventListener("load", function () {
   var pagetoc = document.getElementsByClassName("pagetoc")[0];
   var headers = document.getElementsByClassName("header");
 
+  if (headers.length < 2) {
+    return pagetoc.remove();
+  }
+
   Array.prototype.forEach.call(headers, function (header) {
     var pagetocLink = document.createElement("a");
     var tagName = header.parentElement.tagName.toLowerCase();
@@ -15,40 +19,40 @@ window.addEventListener("load", function () {
 
     pagetoc.appendChild(pagetocLink);
   });
+
+  // Active pagetoc element on scroll
+  window.addEventListener(
+    "scroll",
+    function () {
+      var pagetoc = document.getElementsByClassName("pagetoc")[0];
+      var activeLink = pagetoc.getAttribute("data-active-link");
+
+      // find use position and get activeEl
+      var headers = document.getElementsByClassName("header");
+      var lastHeader;
+      for (var header of headers) {
+        if (window.pageYOffset >= header.offsetTop) {
+          lastHeader = header;
+        } else {
+          break;
+        }
+      }
+      // if same, no need do anything
+      if (activeLink == lastHeader.parentElement.id) {
+        return;
+      }
+      activeLink = lastHeader.parentElement.id;
+      pagetoc.setAttribute("data-active-link", activeLink);
+
+      // Set pagetoc active element
+      Array.prototype.forEach.call(pagetoc.children, function (pagetocLink) {
+        if (activeLink == pagetocLink.getAttribute("data-referrer")) {
+          pagetocLink.classList.add("active");
+        } else {
+          pagetocLink.classList.remove("active");
+        }
+      });
+    },
+    false
+  );
 });
-
-// Active pagetoc element on scroll
-window.addEventListener(
-  "scroll",
-  function () {
-    var pagetoc = document.getElementsByClassName("pagetoc")[0];
-    var activeLink = pagetoc.getAttribute("data-active-link");
-
-    // find use position and get activeEl
-    var headers = document.getElementsByClassName("header");
-    var lastHeader;
-    for (var header of headers) {
-      if (window.pageYOffset >= header.offsetTop) {
-        lastHeader = header;
-      } else {
-        break;
-      }
-    }
-    // if same, no need do anything
-    if (activeLink == lastHeader.parentElement.id) {
-      return;
-    }
-    activeLink = lastHeader.parentElement.id;
-    pagetoc.setAttribute("data-active-link", activeLink);
-
-    // Set pagetoc active element
-    Array.prototype.forEach.call(pagetoc.children, function (pagetocLink) {
-      if (activeLink == pagetocLink.getAttribute("data-referrer")) {
-        pagetocLink.classList.add("active");
-      } else {
-        pagetocLink.classList.remove("active");
-      }
-    });
-  },
-  false
-);
