ce788374887e679511f371faccc64e9cba850e2a
Shueh Chou Lu <wu0dj2k7ao3@gmail.com>
2021-10-14 10:54:14 +0800

    Fix setting

--- src/extra/refresh_on_toggle_dark_light.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("__palette_1").addEventListener("change", function () {
-  location.reload();
-});
-
-document.getElementById("__palette_2").addEventListener("change", function () {
-  location.reload();
-});
