c8ae5c4001e1cf06dc2e50a72b532cb54cb603cf
Shueh Chou Lu <wu0dj2k7ao3@gmail.com>
2022-07-16 12:00:31 +0800

    feat: serve only specific files

---
+++ lib/evan361425/serve_simple.py
@@ -0,0 +1,63 @@
+"""
+Main plugin module for simply serve dev mode
+"""
+
+from os import environ, getpid, path
+from pathlib import Path
+from mkdocs.plugins import BasePlugin
+
+from json import load
+from shutil import copyfile, copytree, rmtree
+
+# ------------------------
+# Constants and utilities
+# ------------------------
+SETTING_NAME = "serve.json"
+
+# ------------------------
+# Plugin
+# ------------------------
+class MarkdownServeSimplePlugin(BasePlugin):
+    def on_config(self, config):
+        # only support on development
+        if (
+            not config["site_url"].startswith("http://127.0.0.1")
+            or environ["MKDOCS_SERVE_ALL"]
+        ):
+            return
+
+        pid = str(getpid())
+        src = config["docs_dir"] + "/"
+
+        setting = self.__parse_setting(config)
+        dest = replace_last_file(config["docs_dir"], setting["dest"])
+        config["docs_dir"] = dest
+
+        # Check exist
+        if path.exists(dest + "/pid"):
+            with open(dest + "/pid", "r") as pidF:
+                if pidF.read() == pid:
+                    return config
+
+        # Recreate
+        rmtree(dest)
+        Path(dest).mkdir(parents=True, exist_ok=True)
+        with open(dest + "/pid", "w") as pidF:
+            pidF.write(pid)
+
+        # copy
+        for target in setting["targets"]:
+            f = copytree if target.endswith("/") else copyfile
+            f(src + target, dest + "/" + target)
+
+        return config
+
+    def __parse_setting(self, config):
+        file_name = replace_last_file(config["config_file_path"], SETTING_NAME)
+        with open(file_name) as json_file:
+            return load(json_file)
+
+
+def replace_last_file(path: str, name: str):
+    index = path.rfind("/")
+    return path[0 : index + 1] + name
